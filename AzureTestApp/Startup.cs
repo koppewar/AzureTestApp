c65d06eae7b64fcf2d775838db4718faefc61b04
karthik koppewar <koppewar123@gmail.com>
2016-11-27 13:26:12 +0530

    first commit

---
+++ AzureTestApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AzureTestApp.Startup))]
+namespace AzureTestApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
